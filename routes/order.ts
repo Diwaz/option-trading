ab6d430687096b9718a5c01e3d8caacab50506dd
Diwaz <diwaswatson@gmail.com>
2025-08-30 02:50:40 +0545

    get open order and post order

--- routes/order.ts
+++ routes/order.ts
@@ -8,27 +8,53 @@ const router = express.Router();
 // In-memory orders
 //  { orderId: { token, price, quantity, userId, type } }
 const orders: Record<string, any> = {};
+interface Trade  {
+  orderId: string,
+  assest: string,
+  openPrice: number
+  margin: number,
+  leverage: number,
+  type: "buy" | "sell",
+}
+// {userId { trades : [ {  trade1 } , { trade2 }] }}
+const openTrades :Record<string,{trades:Trade[]}>={};
 let orderCounter = 1;
-
-// POST /order/open
+const addTrades = (userId: string , trade: Trade) =>{
+ if (!openTrades[userId]) {
+    openTrades[userId] = { trades: [] }; 
+  }
+  openTrades[userId]?.trades.push(trade);
+}
+// POST /trade/
 router.post("/", (req, res) => {
-  const { token, userId, type,leverage,margin } = req.body;
+  const { asset, userId, type,leverage,margin } = req.body;
 
-  if (!token || !userId || !type || !leverage || !margin) {
-    return res.status(400).json({ error: "token, price, quantity, userId, and type are required" });
+  if (!asset || !userId || !type || !leverage || !margin) {
+    return res.status(411).json({ message: "Incorrect inputs" });
   }
 
   if (type !== "buy" && type !== "sell") {
-    return res.status(400).json({ error: "type must be either 'buy' or 'sell'" });
+    return res.status(411).json({ message: "Incorrect inputs" });
   }
 
   const orderId = randomUUIDv7();
-  const buyPrice = prices[token];
+  const buyPrice = prices[asset];
   // console.log("traded at",buyPrice);
-  orders[orderId] = { orderId, token, buyPrice, margin,leverage, userId, type };
-  updateBalanceForUser(userId,token,buyPrice,leverage,margin,type);
-  res.json({  orderId: orderId });
+  orders[orderId] = { orderId, asset,openPrice: buyPrice, margin,leverage, type };
+  updateBalanceForUser(userId,asset,buyPrice,leverage,margin,type);
+  console.log("trades",orders[orderId]);
+  addTrades(userId,orders[orderId]);
+  console.log("openTrades",JSON.stringify(openTrades,null,2));
+
+  res.status(200).json({  orderId: orderId });
 });
+
+router.get('/open',(req,res)=>{
+    const {userId} = req.body;
+  res.json(
+    openTrades[userId] || []
+  )
+})
 
 
 export default router;
